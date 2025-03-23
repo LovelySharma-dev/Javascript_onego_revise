3dbcbb4e1011b21b4ab64c67cac091e883bef8d6
Lovely <lovelysharmacji@gmail.com>
2025-03-23 19:19:26 +0000

    datatypes done

---
+++ 01_BASICS/02_datatypes.js
@@ -0,0 +1,41 @@
+"use strict"; // treat all JS code as newer verison
+
+// alert(3 + 3) 
+// we are using nodejs not browser , in browser its a pop which says 6
+
+console.log(3
+    + 
+    3); // code readbility should be high
+
+console.log("Lovely") // 👍
+
+
+/* DATA TYPES*/
+
+
+let name = "JigglyPuff"
+let age = 19
+let isLoggedIn = true
+let state = "" // string
+let power; // undefined
+let lose = null // null
+
+
+//1. string => ""
+//2. number range=> 2 to power 53
+//3. bigint
+//4. boolean => true / false
+//5. null => standalone value
+//6. undefined => value not assigned yet
+//7. symbol => unique
+
+
+//OBJECT
+
+// know the type of data by typeof
+console.log(typeof "Wintermoon"); // string
+console.log(typeof age); // number h upar 19 assign ki h
+
+console.log(typeof null); //object which is considered as an JS error by many
+console.log(typeof undefined); // undefined
+
