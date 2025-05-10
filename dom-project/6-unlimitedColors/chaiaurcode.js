64983886238939dd68402da0ef20e3a7d82cf327
Lovely <lovelysharmacji@gmail.com>
2025-05-10 18:37:34 +0000

    Bg color changer done

---
+++ dom-project/6-unlimitedColors/chaiaurcode.js
@@ -0,0 +1,31 @@
+//Generate a Random Color
+const randomColor = function(){
+    const hex = "0123456789ABCDEF"
+    let color = '#'
+    for(let i = 0; i < 6; i++){
+        color += hex[Math.floor(Math.random()* 16)]
+    }
+    return color
+}
+
+
+let intervalId;
+const startchangingcolor = function(){
+
+    if(!intervalId){
+        intervalId = setInterval(changebgcolor, 1000)
+    }
+
+    function changebgcolor (){
+        document.body.style.backgroundColor = randomColor();
+    }
+    
+}
+const stopchangingcolor = function(){
+    clearInterval(intervalId);
+    intervalId = null;
+}
+
+document.querySelector('#start').addEventListener('click', startchangingcolor)
+
+document.querySelector('#stop').addEventListener('click', stopchangingcolor)
