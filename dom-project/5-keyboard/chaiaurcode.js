5fa58e336e997b4667b9e0fe6167f0d40d33bd0a
Lovely <lovelysharmacji@gmail.com>
2025-05-11 08:17:01 +0000

    keyboard dom done

--- dom-project/5-keyboard/chaiaurcode.js
+++ dom-project/5-keyboard/chaiaurcode.js
@@ -1,1 +1,19 @@
-console.log('Project 5');
+const keypressed = document.getElementById('insert')
+window.addEventListener('keydown', (e)=>{
+    keypressed.innerHTML =`
+    <div class = 'color'>
+    <table>
+    <tr>
+     <th>Key</th>
+     <th>Keycode</th>
+     <th>Code</th>
+    </tr>
+    <tr>
+     <td>${e.key === " " ? 'Space' :e.key}</td>
+     <td>${e.keyCode}</td>
+     <td>${e.code}</td>
+    </tr>
+    </table>
+    </div>
+    `
+})
